90643d2b0e599795c5284abcb2b984ea539f8197
muhammadzhuhry <muhammadzhuhry@gmail.com>
2019-10-12 16:51:48 +0700

    add function SetJSONResp

--- main.go
+++ main.go
@@ -6,19 +6,29 @@ import (
 	"os"
 )
 
+// SetJSONResp function for return JSON
+func SetJSONResp(res http.ResponseWriter, httpCode int, resMessage []byte) {
+	res.Header().Set("Content-type", "application/json")
+	res.WriteHeader(httpCode)
+	res.Write(resMessage)
+}
+
 func main() {
 
+	// root
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
-		res.Header().Set("Content-type", "application/json")
-		res.WriteHeader(http.StatusOK)
-		res.Write([]byte(`{
+
+		resMessage := []byte(`{
 			"success": true,
 			"data": "index",
 			"message": "This service is running properly",
 			"code": 200
-		}`))
+		}`)
+
+		SetJSONResp(res, http.StatusOK, resMessage)
 	})
 
+	// listen port
 	err := http.ListenAndServe(":9000", nil)
 
 	if err != nil {
